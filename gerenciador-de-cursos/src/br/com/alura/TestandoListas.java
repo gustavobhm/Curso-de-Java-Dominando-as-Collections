f1c59d55301ac15d7a69dd1112817519792414de
Gustavo Luis dos Santos <gustavobhm@bol.com.br>
2019-05-07 09:41:36 -0300

    Aula 1 - Trabalhando com ArrayList

---
+++ gerenciador-de-cursos/src/br/com/alura/TestandoListas.java
@@ -0,0 +1,24 @@
+package br.com.alura;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class TestandoListas {
+
+	public static void main(String[] args) {
+
+		String curso1 = "Java 8: Tire proveito dos novos recursos da linguagem";
+		String curso2 = "Apache Camel";
+		String curso3 = "Certificação Java SE 8 Programmer I";
+
+		ArrayList<String> cursos = new ArrayList<>();
+		cursos.add(curso1);
+		cursos.add(curso2);
+		cursos.add(curso3);
+
+		Collections.sort(cursos);
+
+		System.out.println(cursos);
+	}
+
+}
