52b39c8fd743d53763391eaa079e4a643542648f
Gustavo Luis dos Santos <gustavobhm@bol.com.br>
2019-05-08 09:11:21 -0300

    Aula 4 - Mais práticas com relacionamentos

--- gerenciador-de-cursos/src/br/com/alura/Curso.java
+++ gerenciador-de-cursos/src/br/com/alura/Curso.java
@@ -5,30 +5,40 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Curso {
-	
-    private String nome;
-    private String instrutor;
-    private List<Aula> aulas = new LinkedList<Aula>();
 
-    public Curso(String nome, String instrutor) {
-        this.nome = nome;
-        this.instrutor = instrutor;
-    }
+	private String nome;
+	private String instrutor;
+	private List<Aula> aulas = new LinkedList<Aula>();
 
-    public String getNome() {
-        return nome;
-    }
+	public Curso(String nome, String instrutor) {
+		this.nome = nome;
+		this.instrutor = instrutor;
+	}
 
-    public String getInstrutor() {
-        return instrutor;
-    }
+	public String getNome() {
+		return nome;
+	}
 
-    public List<Aula> getAulas() {
-        return Collections.unmodifiableList(aulas);
-    }
+	public String getInstrutor() {
+		return instrutor;
+	}
 
-    public void adiciona(Aula aula) {
-        this.aulas.add(aula);
-    }
+	public List<Aula> getAulas() {
+		return Collections.unmodifiableList(aulas);
+	}
+
+	public void adiciona(Aula aula) {
+		this.aulas.add(aula);
+	}
+
+	public int getTempoTotal() {
+		return this.aulas.stream().mapToInt(Aula::getTempo).sum();
+	}
+
+	@Override
+	public String toString() {
+		return "[Curso: " + this.getNome() + ", tempo total: " + this.getTempoTotal() + ", aulas: + " + this.aulas
+				+ "]";
+	}
 
 }
