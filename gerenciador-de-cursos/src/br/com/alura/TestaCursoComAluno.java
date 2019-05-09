f1a811340bcf37a3d5d72bbb35167536ca4da2a2
Gustavo Luis dos Santos <gustavobhm@bol.com.br>
2019-05-09 10:10:35 -0300

    Aula 8 - Outros sets e iterators

--- gerenciador-de-cursos/src/br/com/alura/TestaCursoComAluno.java
+++ gerenciador-de-cursos/src/br/com/alura/TestaCursoComAluno.java
@@ -1,5 +1,9 @@
 package br.com.alura;
 
+import java.util.Iterator;
+import java.util.Set;
+import java.util.Vector;
+
 public class TestaCursoComAluno {
 
 	public static void main(String[] args) {
@@ -13,28 +17,42 @@ public class TestaCursoComAluno {
 		Aluno a1 = new Aluno("Rodrigo Turini", 34672);
 		Aluno a2 = new Aluno("Guilherme Silveira", 5617);
 		Aluno a3 = new Aluno("Mauricio Aniche", 17645);
-		
+
 		javaColecoes.matricula(a1);
 		javaColecoes.matricula(a2);
 		javaColecoes.matricula(a3);
-		
+
 		System.out.println("Todos os alunos matriculados: ");
-		javaColecoes.getAlunos().forEach(aluno -> {
-		    System.out.println(aluno);
-		});
-		
+
+		Set<Aluno> alunos = javaColecoes.getAlunos();
+		Iterator<Aluno> iterador = alunos.iterator();
+
+		while (iterador.hasNext()) {
+			System.out.println(iterador.next());
+		}
+
+//		for (Aluno aluno : javaColecoes.getAlunos()) {
+//			System.out.println(aluno);
+//		}
+//
+//		javaColecoes.getAlunos().forEach(aluno -> {
+//			System.out.println(aluno);
+//		});
+
+		Vector<Aluno> vetor = new Vector<>();
+
 		System.out.println("O aluno " + a1.getNome() + " está matriculado?");
 		System.out.println(javaColecoes.estaMatriculado(a1));
-		
+
 		Aluno turini = new Aluno("Rodrigo Turini", 34672);
 		System.out.println("E esse Turini, está matriculado?");
 		System.out.println(javaColecoes.estaMatriculado(turini));
-		
+
 		System.out.println("O a1 é equals ao Turini?");
 		System.out.println(a1.equals(turini));
-		
+
 		System.out.println(a1.hashCode() == turini.hashCode());
-		
+
 	}
 
 }
